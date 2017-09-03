b31341fca66b7876e638d64d72688dc2d82e8ade
pninabsh <pninabsh@gmail.com>
2017-09-03 18:51:34 +0300

    Buttons and a list box were added.

--- taskManager2/taskManager2/MainWindow.xaml.cs
+++ taskManager2/taskManager2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace taskManager2
 {
@@ -23,6 +24,11 @@ namespace taskManager2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
